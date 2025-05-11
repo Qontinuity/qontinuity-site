eca8fed70c29438ed161370ee5af056c734b03c2
JEAN GAULTIER <jeangaultier@8e:93:51:4d:05:30.home>
2025-05-11 17:34:09 +0200

    Format docs and Astro config with Prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
-// https://astro.build/config
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  // Add your domain name here
   site: "https://yourwebsite.com/",
-  integrations: [ sitemap()]
+  integrations: [sitemap()],
 });
